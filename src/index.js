46d124d35d044d5454b3fe33e21b15860b8ea1e6
jasmin-alejandra <jasmin-ag88@hotmail.com>
2025-10-22 16:38:44 -0400

    updated weather data & date format function

--- src/index.js
+++ src/index.js
@@ -2,8 +2,39 @@ function updateWeather(response) {
   let temperatureElement = document.querySelector("#current-temp");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#current-city");
+  let timeElement = document.querySelector("#time");
+  let conditionElement = document.querySelector("#condition");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let date = new Date(response.data.time * 1000);
   temperatureElement.innerHTML = Math.round(temperature);
   cityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = formatDate(date);
+  conditionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windElement.innerHTML = `${response.data.wind.speed}mph`;
+}
+
+function formatDate(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}, `;
 }
 
 function searchCity(city) {
